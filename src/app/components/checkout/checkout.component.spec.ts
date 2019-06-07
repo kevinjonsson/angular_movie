d63c3a95c662deb38f596468956a6292cf6e96ba
kevinjonsson <kevin.jonsson92@gmail.com>
2019-06-07 14:33:09 +0200

    more files that did not get into last commit

---
+++ src/app/components/checkout/checkout.component.spec.ts
@@ -0,0 +1,36 @@
+import { CheckoutComponent } from './checkout.component';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MovieShopComponent } from '../movie-shop/movie-shop.component';
+import { MoviePresentationComponent } from '../movie-presentation/movie-presentation.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShoppingCartComponent } from '../shopping-cart/shopping-cart.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DataService } from 'src/app/services/data.service';
+import { MockDataService } from 'src/app/services/mock-data.service';
+
+
+  describe('CheckoutComponent', () => {
+    let component: CheckoutComponent;
+    let fixture: ComponentFixture<CheckoutComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CheckoutComponent, ShoppingCartComponent, MovieShopComponent, MoviePresentationComponent ],
+      imports: [RouterTestingModule, HttpClientModule],
+      providers: [{provide: DataService, useClass: MockDataService}]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+
+    fixture = TestBed.createComponent(CheckoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
